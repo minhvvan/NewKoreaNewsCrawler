5c516322e2e415147598d1ccd802ed9ba38efae3
unknown <eminhwan12@gmail.com>
2020-11-19 17:36:29 +0900

    test selenium

--- temp.py
+++ temp.py
@@ -1,5 +1,9 @@
 from selenium import webdriver 
 
 browser = webdriver.Chrome('.\chromedriver.exe') 
-browser.get('http://www.google.com/') 
-browser.save_screenshot('screenie.png')
+browser.get('https://finance.naver.com/sise/')
+element = browser.find_element_by_class_name('box_top_submain2')
+# element = browser.find_element_by_id('main_content')
+size = element.size 
+print(size)
+element.screenshot('clcl.png')
