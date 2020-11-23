81decfafbbfaa3f7a13357b62bbd9901db086b23
unknown <eminhwan12@gmail.com>
2020-11-24 00:42:28 +0900

    merge capturePhoto

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     # print(os.getcwd()+'\chromedriver.exe')
     Crawler = ArticleCrawler()
     Crawler.set_category("IT과학")  # 정치, 경제, 생활문화, IT과학, 사회, 세계 카테고리 사용 가능
-    Crawler.set_date_range(2020, 11, 19, 2020, 11, 19)
+    Crawler.set_date_range(2020, 11, 22, 2020, 11, 22)
     Crawler.set_keyword("네이버")
     Crawler.set_captureFlag(True)
     Crawler.start()
