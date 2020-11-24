d96e485875f80242ab6ac4d452c5c632d4d67de7
unknown <eminhwan12@gmail.com>
2020-11-24 21:59:07 +0900

    merge covid

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from korea_news_crawler.articlecrawler import ArticleCrawler
+from korea_news_crawler.covidcrawler import covidcrawler
 import os
 
 if __name__ == "__main__":
     # print(os.getcwd()+'\chromedriver.exe')
-    Crawler = ArticleCrawler()
-    Crawler.set_category("IT과학")  # 정치, 경제, 생활문화, IT과학, 사회, 세계 카테고리 사용 가능
-    Crawler.set_date_range(2020, 11, 22, 2020, 11, 22)
-    Crawler.set_keyword("네이버")
-    Crawler.set_captureFlag(True)
-    Crawler.start()
+    # Crawler = ArticleCrawler()
+    # Crawler.set_category("IT과학")  # 정치, 경제, 생활문화, IT과학, 사회, 세계 카테고리 사용 가능
+    # Crawler.set_date_range(2020, 11, 22, 2020, 11, 22)
+    # Crawler.set_keyword("네이버")
+    # Crawler.set_captureFlag(True)
+    # Crawler.start()
+    covidCrawler = covidcrawler()
+    covidCrawler.getConfirmed()
